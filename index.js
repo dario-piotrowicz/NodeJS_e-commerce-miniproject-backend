b8b0820198e5cb151a18a3a26e63f4920cf23244
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-05-09 20:23:38 +0200

    applied corsOptions to cors()

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
+let corsOptions = {
+    origin: '*'
+};
+app.use(cors(corsOptions));
 
 app.listen( port, error => {
     if( error ) throw error;
