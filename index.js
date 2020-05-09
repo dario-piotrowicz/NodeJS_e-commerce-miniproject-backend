955097f969a8528673acbe65461758c643034df3
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-05-09 17:49:19 +0200

    removed static serving in production as project differs from the one done in the course

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const path = require('path');
 
 if( process.env.NODE_ENV !== 'production' ) require('dotenv').config();
 
@@ -14,14 +13,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
-
-if( process.env.NODE_ENV === 'production' ){
-    app.use(express.static(path.join(__dirname, 'client/build')));
-
-    app.get('*', (req,res) => {
-        res.sendFile(path.join(__dirname,'client/build', 'index.html'));
-    });
-}
 
 app.listen( port, error => {
     if( error ) throw error;
